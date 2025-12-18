6a8c06ccb1bf3ae1344673266bdba85b83abd2c8
x676w <x676w@atomicmail.io>
2025-12-19 01:34:43 +0300

    update logicalExpressionCompiler: now uses label-based jumps

--- src/compiler/compilers/expressions/LogicalExpressionCompiler.ts
+++ src/compiler/compilers/expressions/LogicalExpressionCompiler.ts
@@ -10,37 +10,33 @@ export default class LogicalExpressionCompiler extends NodeCompiler<LogicalExpre
   
   public override compile(node: LogicalExpression): void {
     this.compiler.compileNode(node.left);
-    
-    switch(node.operator) {
+
+    const endLabel = this.compiler.bytecode.createLabel("logical_end");
+
+    switch (node.operator) {
       case "&&": {
         this.compiler.bytecode.writeOperationCode(OperationCode.STACK_PUSH_DUPLICATE);
         this.compiler.bytecode.writeOperationCode(OperationCode.JUMP_IF_FALSE);
-        
-        const jumpPos = this.compiler.bytecode.program.length;
-        this.compiler.bytecode.writeDword(0);
+        this.compiler.bytecode.writeLabelReference(endLabel);
 
         this.compiler.bytecode.writeOperationCode(OperationCode.STACK_POP);
         this.compiler.compileNode(node.right);
 
-        const endPos = this.compiler.bytecode.program.length;
-        this.compiler.bytecode.writeDwordAt(jumpPos, endPos);
+        this.compiler.bytecode.markLabel(endLabel);
         break;
-      };
-    
+      }
+
       case "||": {
         this.compiler.bytecode.writeOperationCode(OperationCode.STACK_PUSH_DUPLICATE);
         this.compiler.bytecode.writeOperationCode(OperationCode.JUMP_IF_TRUE);
-          
-        const jumpPos = this.compiler.bytecode.program.length;
-        this.compiler.bytecode.writeDword(0);
+        this.compiler.bytecode.writeLabelReference(endLabel);
 
         this.compiler.bytecode.writeOperationCode(OperationCode.STACK_POP);
         this.compiler.compileNode(node.right);
 
-        const endPos = this.compiler.bytecode.program.length;
-        this.compiler.bytecode.writeDwordAt(jumpPos, endPos);
+        this.compiler.bytecode.markLabel(endLabel);
         break;
-      };
-    };
+      }
+    }
   };
 };
